ad54115dc053dce1b80fca769cecba0859466546
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-12-12 18:43:15 -0500

    Selection boxes

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const gameBoard = (() => {
         container.appendChild(boardArray['div' + i]);
 
     }
+    return {
+        boardArray
+    };
 
 })();
 
@@ -25,13 +28,16 @@ const gameSelection = (() => {
     const container = document.querySelector('#selection-container')
 
     for (i = 0; i <= 1; i++) {
-        boardArray['div' + i];
+        selectionArray['div' + i];
 
-        boardArray['div' + i] = document.createElement('div');
-        boardArray['div' + i].setAttribute('id', 'board-item' + i);
-        boardArray['div' + i].classList.add('board-item')
-        container.appendChild(boardArray['div' + i]);
+        selectionArray['div' + i] = document.createElement('div');
+        selectionArray['div' + i].setAttribute('id', 'selection-item' + i);
+        selectionArray['div' + i].classList.add('selection-item')
+        container.appendChild(selectionArray['div' + i]);
 
     }
+
+    document.querySelector('#selection-item0').textContent = 'O';
+    document.querySelector('#selection-item1').textContent = 'X';
 
 })();
