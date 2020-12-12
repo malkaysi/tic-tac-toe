13e5add5484dcb4e8b3916eafbc3c4083ebab876
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2020-12-12 15:25:45 -0500

    Board created

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ const gameBoard = (() => {
 
     // Need to create boxes within html element and loop to create 9 of them
     const container = document.querySelector('#board-container')
-    const div = document.createElement('div');
-    div.setAttribute('id', 'board-item');
-    div.classList.add('board-item')
-    container.appendChild(div);
+
+    for (i = 0; i <= 8; i++) {
+        boardArray['div' + i];
+
+        boardArray['div' + i] = document.createElement('div');
+        boardArray['div' + i].setAttribute('id', 'board-item' + i);
+        boardArray['div' + i].classList.add('board-item')
+        container.appendChild(boardArray['div' + i]);
+
+    }
+    console.log(boardArray);
+
     // They sould be added to the boardArray
 })();
 
